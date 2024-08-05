5669791105fb20d500f25459d3ad27edff165dd8
Riqui26 <167150532+Riqui26@users.noreply.github.com>
2024-08-05 19:24:59 -0300

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,51 @@
+const productosDisponibles = [
+    {
+        id: 1,
+        nombre: 'Manzana',
+        precio: 250,
+        categoria: 'Verduleria',
+        stock: 3
+    },
+    {
+        id: 2,
+        nombre: 'Banana',
+        precio: 200,
+        categoria: 'Verduleria',
+        stock: 30
+    },
+    {
+        id: 3,
+        nombre: 'Leche',
+        precio: 100,
+        categoria: 'Lacteos',
+        stock: 50
+    },
+    {
+        id: 4,
+        nombre: 'Pan',
+        precio: 150,
+        categoria: 'Panaderia',
+        stock: 20
+    },
+    {
+        id: 5,
+        nombre: 'Queso',
+        precio: 500,
+        categoria: 'Lacteos',
+        stock: 15
+    },
+    {
+        id: 6,
+        nombre: 'Carne',
+        precio: 150,
+        categoria: 'Carniceria',
+        stock: 20
+    },
+    {
+        id: 7,
+        nombre: 'Facturas',
+        precio: 150,
+        categoria: 'Panaderia',
+        stock: 20
+    }
+];
